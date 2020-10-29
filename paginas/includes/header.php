3166af41f1d2199ecc2e7fd3f7821715fc2f76e0
camila062 <camilalidiaaraujo@gmail.com>
2020-10-29 16:27:20 -0300

    alteraçao

---
+++ paginas/includes/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="public/bootstrap/css/bootstrap.min.css">
+
+    <!-- icomoon -->
+    <link rel="stylesheet" href="public/fonts/icomoon/icomoon.css">
+
+    <!-- css -->
+    <link rel="stylesheet" href="public/meuCssAula9.css">
+
+    <title>Currículo</title>
+</head>
+
+<body style="background-color:  #FAD6B9;">
+    <br> <br>
+    <header class="container text-center py-3">
+        <div class="row">
+            <div class="col">
+
+                <img src="public/img/eu2.jpg" alt="" class="rounded-circle">
+            </div>
+            <div class="col">
+
+                <h3>Camila Lídia de Araújo
+                    <br> <small class="lead">Progrmadora / Estudante</small>
+                </h3>
+                <hr>
+
+                <h5> Perfil
+                    <br> <small>
+                        Idade: 16 anos
+                        <br> Estado Civíl: Solteira
+                        <br> Nacionalidade: Brasileira
+                    </small>
+                </h5>
+                <hr>
+                <br>
+                <h5> <span class="Mila-location"></span>
+                    <small>Reife-PE</small>
+                </h5>
+                <h5> <span class="Mila-phone"></span>
+                    <small>(81) 98738-1882 OI</small>
+                </h5>
+                <h5> <span class="Mila-envelop"></span>
+                    <small>camilalidiaaraujo@gmail.com</small>
+                </h5>
+
+            </div>
+        </div>
+    </header>
+
---
+++ paginas/inicial.php
@@ -0,0 +1,47 @@
+<div class="container">
+
+    <div class="row">
+        <div class="col-3">
+            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Educação</a>
+                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Experiênia</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Qualidades</a>
+            </div>
+        </div>
+        <div class="col-9">
+            <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"></div>
+
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                    <img src="https://placehold.it/200x200" class="rounded float-right" alt="">
+                    <h5 class="text-center">
+                        <small>(2014-2018)</small>
+                        Ensino Fundameltal <br> <span class="lead">Núcleo Educacional Machado de Assis</span>
+                        <hr>
+                        <small>(2019-Atualmene)</small>
+                        Ensino Médio <br> <span class="lead">EREM Maria Vieira Muliterno</span>
+                        <hr>
+                        <small>(Setembro-Atualmete)</small>
+                        Curso Técnico <br> <span class="lead">SENAC</span>
+                    </h5>
+                </div>
+
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                    <h6 class="text-center">Nenhuma</h6>
+                </div>
+
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                    <ul class="list-group list-group-flush noBackground">
+                        <li class="list-group-item noBackground">Prática</li>
+                        <li class="list-group-item noBackground">Objetiva</li>
+                        <li class="list-group-item noBackground">Ótima digitadora</li>
+                        <li class="list-group-item noBackground">Intuitiva</li>
+                        <li class="list-group-item noBackground">Criativa</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
